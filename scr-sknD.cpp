c42a706ab87f7dd7f67f0304be778f4e3cc65477
kam021m <69814018+kam021m@users.noreply.github.com>
2021-03-27 20:18:42 +0100

    Add files via upload

---
+++ scr-sknD.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int string_length, length;
+string n;
+vector<int> onetree;
+int main()
+{
+	cin >> n;
+	string_length = n.length();
+	length = string_length / 2;
+	for (int i = 0; i < length; i++)
+	{
+		cout << n[i] << n[string_length - i - 1];
+	}
+}
